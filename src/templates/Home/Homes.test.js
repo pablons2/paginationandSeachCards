f0db0bc37f4b05e7691a5a8e6091c262a6ae76f0
pablo <pablo.santos@gstecnologia-al.com.br>
2023-05-23 16:37:38 -0300

    refact | blank area app

---
+++ src/templates/Home/Homes.test.js
@@ -0,0 +1,11 @@
+[{
+	"resource": "/c:/Users/pablo.santos/Documents/@reactjs-nextjs/startnewprojeto/src/App.test.js",
+	"owner": "Jest (startnewprojeto)",
+	"severity": 8,
+	"message": "renders learn react link\n-----\nTestingLibraryElementError: Unable to find an element with the text: /learn react/i. This could be because the text is broken up by multiple elements. In this case, you can provide a function for your text matcher to make your matcher more flexible.\n\nIgnored nodes: comments, script, style\n<body>\n  <div>\n    <div\n      class=\"App\"\n    />\n  </div>\n</body>",
+	"source": "Jest",
+	"startLineNumber": 6,
+	"startColumn": 1,
+	"endLineNumber": 6,
+	"endColumn": 9007199254740992
+}]
---
+++ src/utils/load-posts.js
@@ -0,0 +1,11 @@
+export const loadPosts = async () =>{
+    const postsResponse = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = await fetch('https://jsonplaceholder.typicode.com/photos');
+    const [posts, photos] = await Promise.all([postsResponse.json(), photosResponse.json()]);
+
+    const postsAndPhotos = posts.map((post, index) => {
+      return { ...post, cover: photos[index].url };
+    });
+
+        return postsAndPhotos;
+}
